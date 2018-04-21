fcb9e39450a35bc917c0ae6d40e1063f86af1852
Jason Evans <jason@galaxa.co>
2018-04-21 16:08:01 -0700

    add authenticate route

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -11,6 +11,11 @@ export default new Router({
       component: require('@/components/LandingPage').default
     },
     {
+      path: 'authenticate',
+      name: 'authenticate-request',
+      component: require('@/components/Authenticate').default
+    },
+    {
       path: '*',
       redirect: '/'
     }
