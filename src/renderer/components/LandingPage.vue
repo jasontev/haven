e641bc3e1f2c17796dbb0cc35a38abd8448ea94e
Jason Evans <jason@galaxa.co>
2018-04-22 03:56:35 -0700

    show identities & sites

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -3,6 +3,16 @@
     <div class="bg-info text-light p-1 text-center">
       My identities
     </div>
+
+    <div class="card m-2" v-for="(identity, key) in identities" :key="key">
+      <div class="card-header">{{ identity.identity_name }}</div>
+      <div class="card-body">
+        Connected sites:
+        <ul>
+          <li v-for="(site, key) in identity.sites" :key="key">{{ key }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
